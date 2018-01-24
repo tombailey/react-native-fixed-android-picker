4bc5315bcae4ee3fe30b46302b74c8fd8b9fd4c7
Tom Bailey <tombailey1@hotmail.co.uk>
2018-01-24 17:04:08 +0000

    Export picker themes correctly

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import {
   Platform,
 } from 'react-native';
 
-import Picker from './Picker';
+import Picker, {
+  Themes,
+} from './Picker';
+
 
 if (Platform.OS !== 'android') {
   console.warn('You have included react-native-fixed-android-picker on a non-Android device.');
 }
 
+
+export {
+  Themes,
+};
 export default Picker;
